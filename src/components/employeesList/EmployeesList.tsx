db4dcdcb06c453b287e7a46de4377e17de956505
Vladimir Nikulin <mail.jorey@gmail.com>
2024-10-30 23:12:07 +0300

    Сделана тема 12.

--- src/components/employeesList/EmployeesList.tsx
+++ src/components/employeesList/EmployeesList.tsx
@@ -7,12 +7,25 @@ import './employeesListStyles.scss';
 import { PencilIcon, TrashIcon } from '../../assets/icons';
 
 export const EmployeesList: FC<EmployeesListProps> = props => {
-    const { employeesList, onItemClick } = props;
+    const { 
+        employeesList, 
+        onItemClick,
+        onItemDelete,
+        onItemEdit
+    } = props;
     const [selectedUser, setSelectedUser] = useState(0);
 
     const employeeClickHandler = (id: number) => {
         setSelectedUser(id);
         onItemClick && onItemClick(id);
+    }
+
+    const employeeEditHandler = (id: number) => {
+        onItemEdit && onItemEdit(id);
+    }
+
+    const employeeDeleteHandler = (id: number) => {
+        onItemDelete && onItemDelete(id);
     }
 
     const isSelected = (id: number) => selectedUser === id;
@@ -28,8 +41,8 @@ export const EmployeesList: FC<EmployeesListProps> = props => {
                                 {`${user.lastName} ${user.firstName} ${user.midleName ?? ''}`.trim()}
                             </div>
                             <div className='empl-list__item_actions'>
-                                <PencilIcon width={18} height={18} />
-                                <TrashIcon width={18} height={18} />
+                                <PencilIcon width={18} height={18} onClick={() => {employeeEditHandler(user.id)}} />
+                                <TrashIcon width={18} height={18} onClick={() => {employeeDeleteHandler(user.id)}} />
                             </div>
                     </div>
                 );
--- src/pages/department/DepartmentsPage.tsx
+++ src/pages/department/DepartmentsPage.tsx
@@ -1,52 +1,150 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 import { Layout } from '../../components/layouts';
-import { Button, Dialog, DropDown, EmployeesList } from '../../components/';
+import { Button, Dialog, DropDown, EmployeesList, TextField } from '../../components/';
 
 import './departmentsPageStyles.scss'
 
+import { Employee, Department } from '../../types/models';
+import { DropDownItem } from '../../components/dropDown/DropDownProps';
+
+const fakeEmployeesData = [
+    { id: 1, lastName: 'Иванов', firstName: 'Иван', midleName: 'Иванович', birthDate: new Date().toISOString(), email: 'ivanov@gmail.com', phoneNumber: '8-800-535-35-35'},
+    { id: 2, lastName: 'Петров', firstName: 'Сергей', midleName: 'Дмитриевич', birthDate: new Date().toISOString(), email: 'petrovv@gmail.com', phoneNumber: '8-800-535-35-36' },
+    { id: 3, lastName: 'Сильверхэнд', firstName: 'Джонни', birthDate: new Date().toISOString(), email: 'silverhand@gmail.com', phoneNumber: '8-800-535-35-37' },
+    { id: 4, lastName: 'Рябчикова', firstName: 'Лидия', midleName: 'Анатольевна', birthDate: new Date().toISOString(), email: 'ryabchikova@gmail.com', phoneNumber: '8-800-535-35-38' },
+    { id: 5, lastName: 'Семенов', firstName: 'Олег', midleName: 'Артемович', birthDate: new Date().toISOString(), email: 'semenov@gmail.com', phoneNumber: '8-800-535-35-39' },
+] as Array<Employee>;
+
+const fakeDepartmentsData = [
+    {
+        id: 1, name: 'Отдел 1', employees: []
+    },
+    {
+        id: 2, name: 'Отдел 2', employees: fakeEmployeesData
+    },
+    {
+        id: 3, name: 'Отдел 3', employees: []
+    },
+] as Array<Department>;
+
 export const DepartmentsPage: FC = () => {
+    const [departmentData, setDepartmentData] = useState<Array<Department>>([]);
+    const [employeesData, setEmployeesData] = useState<Array<Employee>>([]);
+    
+    const [selectedDepartmentId, setSelectedDepartmentId] = useState<number>();
+
+    const [selectedEmployeeId, setSelectedEmployeeId] = useState<number>();
     const [showEmployeeDialog, setShowEmployeeDialog] = useState(false);
     const [userActionMode, setUserActionMode] = useState<'create' | 'edit'>('create');
+    const [userToEdit, setUserToEdit] = useState(0);
+
+    const [lastName, setLastName] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [midleName, setMidleName] = useState('');
+
+    useEffect(() => {
+        setTimeout(() => {
+            setDepartmentData(fakeDepartmentsData);
+            if (Array.isArray(fakeDepartmentsData) && fakeDepartmentsData.length) {
+                setEmployeesData(fakeDepartmentsData[0].employees);
+            }
+        }, 500);
+    }, []);
+
+    useEffect(() => {
+        const selectedDepartment = departmentData.find(d => d.id === selectedDepartmentId);
+        setEmployeesData(selectedDepartment ? selectedDepartment.employees : []);
+        setSelectedEmployeeId(undefined);
+    }, [departmentData, selectedDepartmentId]);
+
+    useEffect(() => {
+        setEmployeesData(fakeEmployeesData);
+    }, []);
+
+    useEffect(() => {
+        clearEmployeeDialogFields();
+
+        if (userActionMode === 'edit') {
+            const employee = userActionMode === 'edit'
+                ? employeesData.find(e => e.id === userToEdit)
+                : undefined;
+
+            setLastName(employee?.lastName ?? '');
+            setFirstName(employee?.firstName ?? '');
+            setMidleName(employee?.midleName ?? '');
+        }
+    }, [employeesData, userActionMode, userToEdit]);
+
+    const clearEmployeeDialogFields = () => {
+        setLastName('');
+        setFirstName('');
+        setMidleName('');
+    }
 
     const createEmployeeHandler = () => {
         setUserActionMode('create');
         setShowEmployeeDialog(true);
     }
 
+    const editEmployeeHandler = (id: number) => {
+        setUserActionMode('edit');
+        setUserToEdit(id);
+        setShowEmployeeDialog(true);
+    }
+
+    const userDialogContentRenderer = () => {
+        return (
+            <>
+                <TextField labelText='Фамилия' value={lastName} onChange={(val) => setLastName(val)} />
+                <TextField labelText='Имя' value={firstName} onChange={(val) => setFirstName(val)} />
+                <TextField labelText='Отчество' value={midleName} onChange={(val) => setMidleName(val)} />
+            </>
+        );
+    }
+
+    const closeEmployeeDialogHandler = () => {
+        setShowEmployeeDialog(false);
+        clearEmployeeDialogFields();
+    }
+
+    const departmentChangedHandler = (id?: string) => {
+        const _id: number | undefined = !id ? undefined : +id;
+        setSelectedDepartmentId(_id);
+    }
+
+    const onEmployeeSelectedHandler = (id: number) => {
+        setSelectedEmployeeId(id);
+    }
+
     return (
         <Layout>
+            <Dialog title={userActionMode !== 'edit' ? 'Добавить сотрудника' : 'Изменить сотрудника'}
+                open={showEmployeeDialog}
+                onSave={() => {}}
+                onCancel={closeEmployeeDialogHandler}
+            >
+                {userDialogContentRenderer()}   
+            </Dialog>
             <div className='dep-page'>
                 <div className='dep-page__users-list-container'>
-                    <DropDown items={[
-                        {
-                            text: 'Отдел 1', value: '1'
-                        },
-                        {
-                            text: 'Отдел 2', value: '2'
-                        },
-                        {
-                            text: 'Отдел 3', value: '3'
-                        },
-                    ]} label='Отделы:' selectedChanged={(val) => console.log(val)} />
-                    <EmployeesList employeesList={[
-                            { id: 1, lastName: 'Иванов', firstName: 'Иван', midleName: 'Иванович' },
-                            { id: 2, lastName: 'Петров', firstName: 'Сергей', midleName: 'Дмитриевич' },
-                            { id: 3, lastName: 'Джон', firstName: 'Смитт', },
-                            { id: 4, lastName: 'Рябчикова', firstName: 'Лидия', midleName: 'Анатольевна' },
-                            { id: 5, lastName: 'Семенов', firstName: 'Олег', midleName: 'Артемович' },
-                        ]} onItemClick={(id) => console.log(id)}
+                    <DropDown items={departmentData.map(dd => {
+                        return {
+                            text: dd.name,
+                            value: dd.id.toString()
+                        } as DropDownItem;
+                    })}
+                        label='Отделы:'
+                        selectedChanged={(val) => departmentChangedHandler(val)}
                     />
-                    <Button text="Добавить сотрудника" className='dep-page__add-user-btn' />
+                    <EmployeesList employeesList={employeesData}
+                        onItemClick={(id) => onEmployeeSelectedHandler(id)}
+                        onItemDelete={(id) => console.log('delete', id)}
+                        onItemEdit={editEmployeeHandler}
+                    />
+                    <Button text="Добавить сотрудника" className='dep-page__add-user-btn' onClick={createEmployeeHandler} />
                 </div>
                 <div>
-                    <Dialog title={userActionMode !== 'edit' ? 'Добавить сотрудника' : 'Изменить сотрудника'}
-                        open={showEmployeeDialog}
-                        onSave={() => {}}
-                        onCancel={() => setShowEmployeeDialog(false)}
-                    >
-                        
-                    </Dialog>
                     <div>
                         <span>ФИО</span>
                         <div>*</div>
